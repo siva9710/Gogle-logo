fc8addf30e09718841e590dede354f35273aba56
siva <ssiva9710@gmail.com>
2022-05-01 12:42:00 +0530

    first commit

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-print("hi")
-int a = 2
+<DocumentType html>
+<html>
+   <head>
+        <link rel="stylesheet" href="logos.css"> <title>Google Logo</title>
+
+    </head>   
+    <div class="logo">
+    </div>
+</html>
+
+
--- test2.py
+++ test2.py
@@ -1,2 +1,11 @@
-print("HI2")
-print("hello")
+<DocumentType html>
+<html>
+   <head>
+        <link rel="stylesheet" href="logos.css"> <title>Google Logo</title>
+
+    </head>   
+    <div class="logo">
+    </div>
+</html>
+
+
